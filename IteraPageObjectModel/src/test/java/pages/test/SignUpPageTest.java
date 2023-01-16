a71a53f5aaf8798f8d5d669e60a182f4c93c5cf5
Salimahaque <111101710+Salimahaque@users.noreply.github.com>
2023-01-15 21:19:36 -0500

    Add files via upload

---
+++ IteraPageObjectModel/src/test/java/pages/test/SignUpPageTest.java
@@ -0,0 +1,17 @@
+package pages.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
+
+public class SignUpPageTest {
+	
+@BeforeTest
+
+}
+
+	
+}
